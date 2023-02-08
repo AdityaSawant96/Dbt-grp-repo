c16dcfeb7af5a4a0503425f5e9d4e23bdd06fc71
AdityaSawant96 <adiwant996@gmail.com>
2023-02-08 13:38:35 +0530

    First Commit of Swaping number

---
+++ DemoProject/src/sample1/ArthmaticOperation.java
@@ -0,0 +1,17 @@
+package sample1;
+
+public class ArthmaticOperation {
+
+	public static void main(String[] args) {
+	int a= 20,b=15,c;
+	System.out.println("value of a and b before Swapping :-"+a+" ,"+b);
+	c=a;
+	a=b;
+	b=c;
+	System.out.println("value of a and b After Swapping :-"+a+" ,"+b);
+
+		
+		
+	}
+
+}
